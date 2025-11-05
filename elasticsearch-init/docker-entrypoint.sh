1b32f7ace9442834d4cb429a3a25e607b96699cc
Mathieu Poignet <mathieu@control.alt.coop>
2025-11-05 11:18:31 +0100

    Add the entrypoint for ES

--- elasticsearch-init/docker-entrypoint.sh
+++ elasticsearch-init/docker-entrypoint.sh
@@ -13,4 +13,4 @@ echo "Pushing place template to ElasticSearch..."
 curl http://$ELASTICSEARCH_CONTAINER_NAME:9200/_template/gazetteer_template -X PUT -H "Content-Type: application/json" -d "@/mappings/place_template.json"
 
 echo "Create the index..."
-# curl -X PUT "http://$ELASTICSEARCH_CONTAINER_NAME:9200/gazetteer"
+curl -X PUT "http://$ELASTICSEARCH_CONTAINER_NAME:9200/gazetteer"
