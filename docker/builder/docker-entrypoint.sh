89ddbda07d5d74d0349beb9369f43d6023ac3728
Felix Höffken <felix@ctrl.alt.coop>
2025-11-06 09:03:46 +0100

    Run gazetteer on root

--- docker/builder/docker-entrypoint.sh
+++ docker/builder/docker-entrypoint.sh
@@ -9,11 +9,11 @@ cp src/main/webapp/WEB-INF/mail.properties.template src/main/webapp/WEB-INF/mail
 sed -i 's|/var/log/gazetteer/gazetteer.log|logs/gazetteer.log|g' src/main/resources/log4j2.xml
 
 mvn clean package -Dmaven.test.skip=true
-cp /src/target/gazetteer.war /src/.tomcat/gazetteer.war
+cp /src/target/gazetteer.war /src/.tomcat/ROOT.war
 
 inotifywait -r -m -e modify /src/src | while read file_path file_event file_name; do 
     echo ${file_path}${file_name} event: ${file_event}
     echo "Skipping $(timeout 3 cat | wc -l) further changes"
     mvn clean package -Dmaven.test.skip=true
-    cp /src/target/gazetteer.war /src/.tomcat/gazetteer.war
+    cp /src/target/gazetteer.war /src/.tomcat/ROOT.war
 done 
