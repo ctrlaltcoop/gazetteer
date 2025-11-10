525f1d9b41a80fea750befa5749ebb3c49491746
Felix Höffken <felix@ctrl.alt.coop>
2025-11-10 14:28:26 +0100

    Correctly reindex on creation

--- docker/elasticsearch-init/docker-entrypoint.sh
+++ docker/elasticsearch-init/docker-entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-echo "ES Init..."
-echo $ELASTICSEARCH_CONTAINER_NAME
-sleep 5
-
-until $(curl -o /dev/null -s --head --fail $ELASTICSEARCH_CONTAINER_NAME:9200); do
-    echo "Waiting for ES to start..."
-    sleep 5
-done
-
 echo "Pushing place template to ElasticSearch..."
 curl http://$ELASTICSEARCH_CONTAINER_NAME:9200/_template/gazetteer_template -X PUT -H "Content-Type: application/json" -d "@/mappings/place_template.json"
 
@@ -16,4 +6,4 @@ echo "Create the index..."
 curl -X PUT "http://$ELASTICSEARCH_CONTAINER_NAME:9200/gazetteer"
 
 echo "Reindexing…"
-curl -XPOST admin:password@localhost:8080/admin/reindex
+curl -XPOST admin:password@$TOMCAT_CONTAINER_NAME:8080/admin/reindex
