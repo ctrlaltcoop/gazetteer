d15a2a550f490f25c4919f99cd705021efc4c0c3
Felix Höffken <felix@ctrl.alt.coop>
2025-11-10 13:51:54 +0100

    Remove whitespace

--- docker/builder/docker-entrypoint.sh
+++ docker/builder/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 mkdir -p /src/.tomcat/
 rm /src/.tomcat/gazetteer.war
 
-cp src/main/resources/log4j2.xml.template src/main/resources/log4j2.xml 
+cp src/main/resources/log4j2.xml.template src/main/resources/log4j2.xml
 cp src/main/webapp/WEB-INF/web.xml.template src/main/webapp/WEB-INF/web.xml
 cp src/main/webapp/WEB-INF/config.properties.template src/main/webapp/WEB-INF/config.properties
 cp src/main/webapp/WEB-INF/mail.properties.template src/main/webapp/WEB-INF/mail.properties
@@ -10,10 +10,12 @@ sed -i 's|/var/log/gazetteer/gazetteer.log|logs/gazetteer.log|g' src/main/resour
 
 mvn clean package -Dmaven.test.skip=true
 cp /src/target/gazetteer.war /src/.tomcat/ROOT.war
+cp /src/target/gazetteer.war /src/.tomcat/gazetteer.war
 
-inotifywait -r -m -e modify /src/src | while read file_path file_event file_name; do 
+inotifywait -r -m -e modify /src/src | while read file_path file_event file_name; do
     echo ${file_path}${file_name} event: ${file_event}
     echo "Skipping $(timeout 3 cat | wc -l) further changes"
     mvn clean package -Dmaven.test.skip=true
     cp /src/target/gazetteer.war /src/.tomcat/ROOT.war
-done 
+    cp /src/target/gazetteer.war /src/.tomcat/gazetteer.war
+done
