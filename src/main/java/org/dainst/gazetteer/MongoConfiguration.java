85e8880a86048b3e695071d40f9cf3a373cea42e
Felix Höffken <felix@ctrl.alt.coop>
2025-11-18 16:28:27 +0100

    Remove duplicate websecurity config

--- src/main/java/org/dainst/gazetteer/MongoConfiguration.java
+++ src/main/java/org/dainst/gazetteer/MongoConfiguration.java
@@ -1,27 +1,15 @@
 package org.dainst.gazetteer;
 import org.dainst.gazetteer.dao.PlaceRepository;
-import org.dainst.gazetteer.dao.UserPasswordChangeRequestRepository;
-import org.dainst.gazetteer.dao.UserRepository;
-import org.dainst.gazetteer.helpers.AuthenticationSuccessHandler;
 import org.dainst.gazetteer.helpers.MongoBasedIncrementingIdGenerator;
 import org.dainst.gazetteer.helpers.SimpleMerger;
-import org.dainst.gazetteer.helpers.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.WriteResultChecking;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.DelegatingFilterProxy;
-
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoClients;
 
 
 @Configuration
@@ -29,7 +17,6 @@ import com.mongodb.client.MongoClients;
 @EnableWebSecurity
 public class MongoConfiguration extends AbstractMongoConfiguration {
 
-    // region mongo
     @Bean
     public MongoClient mongoClient() {
         return new MongoClient();
@@ -60,73 +47,4 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
         merger.setPlaceRepository(placeRepository);
         return merger;
     }
-
-    // endregion
-
-    // region web security
-    @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler(
-        UserRepository userRepository,
-        UserPasswordChangeRequestRepository userPasswordChangeRequestRepository
-    ) {
-        AuthenticationSuccessHandler authenticationSuccessHandler = new AuthenticationSuccessHandler();
-        authenticationSuccessHandler.setDefaultTargetUrl("/");
-        authenticationSuccessHandler.setTargetUrlParameter("spring-security-redirect");
-        authenticationSuccessHandler.setUserRepository(userRepository);
-        authenticationSuccessHandler.setUserPasswordChangeRequestRepository(userPasswordChangeRequestRepository);
-        return authenticationSuccessHandler;
-    }
-    @Bean
-    public UserService userService(UserRepository userRepository) {
-        UserService userService = new UserService();
-        userService.setUserRepository(userRepository);
-        return userService;
-    }
-
-    @Bean 
-    public PasswordEncoder passwordEncoder() { 
-        return new BCryptPasswordEncoder(); 
-    }
-
-    @Bean
-    public SecurityFilterChain springSecurityFilterChain(
-        HttpSecurity http,
-        AuthenticationSuccessHandler authenticationSuccessHandler
-    ) throws Exception {
-        http.csrf(csrf ->
-                csrf.disable()
-            )
-            .logout(logout ->
-                 logout.logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-            )
-            .formLogin(form ->
-                    form.loginPage("/login")
-                        .loginProcessingUrl("/j_spring_security_check")
-                        .failureUrl("/loginfailed")
-                        .successHandler(authenticationSuccessHandler)
-            ).authorizeHttpRequests( httpRequest ->
-                httpRequest
-                    .requestMatchers("/user").hasRole("ROLE_USER")
-                    .requestMatchers(HttpMethod.POST, "/doc/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers(HttpMethod.PUT, "/doc/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers(HttpMethod.DELETE, "/doc/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers(HttpMethod.POST, "/merge/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers(HttpMethod.POST, "/duplicate/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers(HttpMethod.POST, "/validation/**").hasRole("ROLE_EDITOR")
-                    .requestMatchers("/admin/**").hasRole("ROLE_ADMIN")
-                    .requestMatchers("/userManagement/**").hasRole("ROLE_ADMIN")
-                    .requestMatchers("/recordGroupManagement/**").hasRole("ROLE_USER")
-                    .requestMatchers("/recordGroupUserManagement/**").hasRole("ROLE_USER")
-                    .requestMatchers("/editUser/**").hasRole("ROLE_USER")
-                    .requestMatchers("/globalChangeHistory/**").hasRole("ROLE_EDITOR")
-            )
-            .addFilter(new DelegatingFilterProxy())
-            // TODO move to DSL?!
-            .httpBasic();
-          
-        return http.build();
-    }
-
-    // endregion
 }
