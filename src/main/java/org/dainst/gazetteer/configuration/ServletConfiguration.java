3c3c3423b68fc2cadadcdd9557e0c3a1634cc97c
Felix Höffken <felix@ctrl.alt.coop>
2025-11-19 14:40:59 +0100

    Migrate more config to java

--- src/main/java/org/dainst/gazetteer/configuration/ServletConfiguration.java
+++ src/main/java/org/dainst/gazetteer/configuration/ServletConfiguration.java
@@ -1,7 +1,9 @@
 package org.dainst.gazetteer.configuration;
 import org.dainst.gazetteer.converter.JsonPlaceMessageConverter;
 import org.dainst.gazetteer.converter.KmlPlaceMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ResourceHttpMessageConverter;
@@ -11,15 +13,48 @@ import org.springframework.http.converter.support.AllEncompassingFormHttpMessage
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebMvc
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class ServletConfiguration implements WebMvcConfigurer {
+
+    private final String CONTENT_TYPE_HTML = "text/html";
+    private final String CONTENT_TYPE_KML = "application/vnd.google-earth.kml+xml";
+    private final String CONTENT_TYPE_JSON = "application/json";
+    private final String CONTENT_TYPE_GEOJSON = "application/vnd.geo+json";
+    private final String CONTENT_TYPE_JS = "application/javascript";
+    private final String CONTENT_TYPE_RDF = "application/rdf+xml";
+
+    @Bean
+    public Map<String, String> mediaTypes() {
+        return Map.of(
+                "html", CONTENT_TYPE_HTML,
+                "kml", CONTENT_TYPE_KML,
+                "json", CONTENT_TYPE_JSON,
+                "geojson", CONTENT_TYPE_GEOJSON,
+                "js", CONTENT_TYPE_JS,
+                "rdf", CONTENT_TYPE_RDF
+        );
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.
+                defaultContentType(
+                        MediaType.parseMediaType(CONTENT_TYPE_HTML),
+                        MediaType.parseMediaType(CONTENT_TYPE_KML),
+                        MediaType.parseMediaType(CONTENT_TYPE_JSON),
+                        MediaType.parseMediaType(CONTENT_TYPE_GEOJSON),
+                        MediaType.parseMediaType(CONTENT_TYPE_JS),
+                        MediaType.parseMediaType(CONTENT_TYPE_RDF)
+                );
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> messageConverters) {
@@ -36,4 +71,61 @@ public class ServletConfiguration implements WebMvcConfigurer {
         ));
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver htmlResolver = new InternalResourceViewResolver(
+        "/WEB-INF/views/html/",
+        ".jsp"
+        );
+        htmlResolver.setContentType(CONTENT_TYPE_HTML);
+
+        InternalResourceViewResolver jsonResolver = new InternalResourceViewResolver(
+                "/WEB-INF/views/json/",
+                ".jsp"
+        );
+        jsonResolver.setContentType(CONTENT_TYPE_JSON);
+
+        InternalResourceViewResolver geoJsonResolver = new InternalResourceViewResolver(
+                "/WEB-INF/views/geojson/",
+                ".jsp"
+        );
+        geoJsonResolver.setContentType(CONTENT_TYPE_GEOJSON);
+
+        InternalResourceViewResolver kmlResolver = new InternalResourceViewResolver(
+                "/WEB-INF/views/kml/",
+                ".jsp"
+        );
+        kmlResolver.setContentType(CONTENT_TYPE_KML);
+
+        InternalResourceViewResolver jsResolver = new InternalResourceViewResolver(
+                "/WEB-INF/views/javascript/",
+                ".jsp"
+        );
+        jsResolver.setContentType(CONTENT_TYPE_JS);
+
+        InternalResourceViewResolver rdfResolver = new InternalResourceViewResolver(
+                "/WEB-INF/views/rdf/",
+                ".jsp"
+        );
+        rdfResolver.setContentType(CONTENT_TYPE_RDF);
+
+        InternalResourceViewResolver jspResolver = new InternalResourceViewResolver(
+               "/WEB-INF/views/",
+               ".jsp"
+        );
+
+
+        registry.viewResolver(htmlResolver);
+        registry.viewResolver(jsonResolver);
+        registry.viewResolver(geoJsonResolver);
+        registry.viewResolver(kmlResolver);
+        registry.viewResolver(jsResolver);
+        registry.viewResolver(rdfResolver);
+        registry.viewResolver(jspResolver);
+    }
 }
