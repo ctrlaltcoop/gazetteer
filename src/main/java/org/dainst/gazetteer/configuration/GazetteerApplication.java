53fc4cc5ff9586c88dcadd39f67cba981eb83388
Felix Höffken <felix@ctrl.alt.coop>
2025-11-20 19:24:22 +0100

    Add GazetteerApplication

---
+++ src/main/java/org/dainst/gazetteer/configuration/GazetteerApplication.java
@@ -0,0 +1,20 @@
+package org.dainst.gazetteer.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "org.dainst.gazetter")
+public class GazetteerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GazetteerApplication.class);
+    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazetteerApplication.class, args);
+	}
+}
