2228ab7cc50025ed6509f4b576e8282cc36e3f5e
Felix Höffken <felix@ctrl.alt.coop>
2025-11-24 11:36:27 +0100

    Remove ElasticSearchConfiguration

--- src/main/java/org/dainst/gazetteer/search/ElasticSearchClientProvider.java
+++ src/main/java/org/dainst/gazetteer/search/ElasticSearchClientProvider.java
@@ -4,7 +4,10 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class ElasticSearchClientProvider {
 
 	@Value("${cluster.name}")
--- src/main/java/org/dainst/gazetteer/search/ElasticSearchIndexer.java
+++ src/main/java/org/dainst/gazetteer/search/ElasticSearchIndexer.java
@@ -12,7 +12,9 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ElasticSearchIndexer {
 	
 	private final static Logger logger = LoggerFactory.getLogger("org.dainst.gazetteer.ElasticSearchIndexer");
